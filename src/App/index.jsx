ad2b535ae6991cbf75f401ff5fe29a83fb7804c6
maestro5696 <westlord56@gmail.com>
2019-09-13 00:16:42 +0300

    basic tempalte

---
+++ src/App/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          app
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/scenes/User/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class User extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        User
+      </div>
+    )
+  }
+}
+
+export default User;
